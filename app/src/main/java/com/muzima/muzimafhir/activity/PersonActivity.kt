fd825e7f7f243943151bff31c6bf2d738a48b72b
Kolbein <kolbeinfoldoy@gmail.com>
2020-01-28 14:19:13 +0100

    Begynt på mutation. Fått til en del, men ikkje siste steget å sende den. mutation PersonCreate() tar Person_Input som parameter or ikkje Person, som er litt problematisk.

--- app/src/main/java/com/muzima/muzimafhir/activity/PersonActivity.kt
+++ app/src/main/java/com/muzima/muzimafhir/activity/PersonActivity.kt
@@ -9,6 +9,8 @@ import com.muzima.muzimafhir.data.AppClient
 import com.muzima.muzimafhir.data.Person
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_person.*
+import typeFixFolder.type.Person_Enum_input
+import typeFixFolder.type.Person_Input
 
 class PersonActivity : AppCompatActivity() {
 
@@ -16,6 +18,7 @@ class PersonActivity : AppCompatActivity() {
     lateinit var mAdapter: FhirViewAdapter
     lateinit var mLayoutManager: LinearLayoutManager
     lateinit var generatePerson : Button
+    lateinit var createPerson : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +27,11 @@ class PersonActivity : AppCompatActivity() {
         generatePerson = generatePersonBtn
         generatePerson.setOnClickListener{
             generatePerson()
+        }
+
+        createPerson = PersonCreateBtn
+        createPerson.setOnClickListener {
+
         }
 
         mLayoutManager = LinearLayoutManager(this)
@@ -35,7 +43,7 @@ class PersonActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        var  appClient = AppClient()
+        //var  appClient = AppClient()
         //appClient.getPerson("5dcaa7f02eb0b70e90761396") { s: String, p: Person -> callbackFunc(s, p)}
 
     }
@@ -51,8 +59,26 @@ class PersonActivity : AppCompatActivity() {
     }
 
     private fun generatePerson() {
+
+        var appClient = AppClient()
+
+        /*
+        appClient.getPerson("5dc2c3b0b16c6834d80bed75") { s: String, p: Person -> callbackFunc(s, p)}
+
         var p1: Person = Person(null, null, null, "male", null, null, null, null)
         persons.add(p1)
         mAdapter.notifyDataSetChanged()
+         */
+
     }
+
+    /*private fun createPerson() {
+        // create person
+        var p : Person = Person(null, null, null, "male", null, null, null, null)
+        var pResourceType : Person_Enum_input = Person_Enum_input.PERSON
+        var pInput : Person_Input = Person_Input(pResourceType, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null)
+
+        var appClient = AppClient()
+        appClient.createPerson(pInput){ s: String, p: Person -> callbackFunc(s, p)}
+    }*/
 }
