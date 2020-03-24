c1727401ca548716c899bc882641e25da09c84c1
Kolbein <kolbeinfoldoy@gmail.com>
2020-03-24 14:27:46 +0100

    Separated some functions that cluttered the IntentActivity into an IntentHandler

--- app/src/main/java/com/muzima/muzimafhir/activity/IntentActivity.kt
+++ app/src/main/java/com/muzima/muzimafhir/activity/IntentActivity.kt
@@ -1,5 +1,6 @@
 package com.muzima.muzimafhir.activity
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,6 +11,7 @@ import com.muzima.muzimafhir.data.fhir.Patient
 import com.muzima.muzimafhir.fhir.dao.PatientDao
 import com.muzima.muzimafhir.fhir.dao.implementation.PatientDaoImpl
 import com.muzima.muzimafhir.fhir.query.PatientQuery
+import com.muzima.muzimafhir.intenthandling.IntentHandler
 import kotlinx.coroutines.*
 import java.util.*
 import kotlin.coroutines.CoroutineContext
@@ -26,53 +28,15 @@ class IntentActivity : AppCompatActivity() {
 
         patientDao = PatientDaoImpl()
 
-        // Get a single patient or patient list
-        if (intent?.action == "com.muzima.muzimafhir.ACTION_REQUEST_RESOURCE") {
-            if (intent.getStringExtra("resourceType") == "patient"
-                    && intent.getStringExtra("queryType") == "getOne") {
+        var intentHandler = IntentHandler()
 
-                var id = intent.getStringExtra("id")
-                var resultIntent = getPatientWithIntent(id)
-                setResult(RESULT_OK, resultIntent)
-                finish()
-            }
-            if (intent.getStringExtra("resourceType") == "patient"
-                    && intent.getStringExtra("queryType") == "getAll") {
-
-                var resultIntent = getPatientListWithIntent()
-                setResult(RESULT_OK, resultIntent)
-                finish()
-            }
+        var resultIntent = intentHandler.handleIntent(intent)
+        if (resultIntent != null) {
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        } else {
+            setResult(RESULT_CANCELED)
+            finish()
         }
-    }
-
-    private fun getPatientWithIntent(id: String): Intent {
-        lateinit var patient: Patient
-        var resultIntent = Intent()
-
-        runBlocking {
-            patient = patientDao.getPatient(id)
-        }
-        var patientJson = gson.toJson(patient)
-
-        resultIntent.putExtra("resource", patientJson)
-        resultIntent.putExtra("queryType", "getOne")
-        resultIntent.putExtra("resourceType", "patient")
-        return resultIntent
-    }
-
-    private fun getPatientListWithIntent(): Intent {
-        lateinit var patientList: List<Patient>
-        var resultIntent = Intent()
-
-        runBlocking {
-            patientList = patientDao.getPatientList()
-        }
-        var patientListJson = gson.toJson(patientList)
-
-        resultIntent.putExtra("resource", patientListJson)
-        resultIntent.putExtra("queryType", "getAll")
-        resultIntent.putExtra("resourceType", "patient")
-        return resultIntent
     }
 }
